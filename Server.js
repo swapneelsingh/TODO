6f69c17bdc121b4471cc7d372b0e70f81073b5b8
swapneelsingh <swapneelsingh14@gmail.com>
2025-04-26 14:16:13 +0530

    changes

--- Server.js
+++ Server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is running on PORT: ${PORT}`);
+      console.log(`Server is running on PORT huahha: ${PORT}`);
     });
   })
   .catch((error) => {
